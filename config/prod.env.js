640a9c9e51befcdabadfe42c480dcdc028bb98e1
代东东 <daidongdong@daidongdongdeMacBook-Air.local>
2018-06-22 18:00:32 +0800

    add

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"https://yt-test.xiaoheiban.cn/api"',
-  BASE_API: '"https://yt.xiaoheiban.cn/api"',
+  BASE_API: '"https://yt-test.xiaoheiban.cn/api"',
+  // BASE_API: '"https://yt.xiaoheiban.cn/api"',
   // BASE_API: '"http://school-dev.xiaoheiban.cn"',
   // BASE_API: '"https://school.xiaoheiban.cn"',
 }
