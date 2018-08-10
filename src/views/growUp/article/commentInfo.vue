3a1c91e07f673027664e1c7e43da26a2e18958dd
CatSeason <1521610560@qq.com>
2018-08-10 10:11:48 +0800

    growup

--- src/views/growUp/article/commentInfo.vue
+++ src/views/growUp/article/commentInfo.vue
@@ -1,13 +1,61 @@
 <template>
-  <div></div>
+  <div class="comment-info content">
+  	<h3>文章标题</h3>
+
+  	<h5>2017-10-07 22:33</h5>
+
+  	<el-radio-group v-model="tabPosition" style="margin-bottom: 30px;">
+	  <el-radio-button label="0">全部评论（50）</el-radio-button>
+	  <el-radio-button label="1">已回复（30）</el-radio-button>
+	  <el-radio-button label="2">待回复（30）</el-radio-button>
+	  <el-radio-button label="3">审核通过（30）</el-radio-button>
+	  <el-radio-button label="4">待审核（30）</el-radio-button>
+	</el-radio-group>
+
+	<div class="comment">
+	  <ul class="comment-items">
+	  	<li>
+	  		<div class="user-info">
+	  			<img src="../../../assets/imgs/44650116_p0.png" alt="">
+	  			特朗普你吧
+	  		</div>
+	  	</li>
+	  </ul>
+	</div>
+
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'commentInfo'
+    name: 'commentInfo',
+    data() {
+      return {
+        tabPosition: '1',
+        ruleForm: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped rel="stylesheet/scss" lang="scss">
+  .comment-items{
+  	.user-info{
 
+      img{
+  		width: 60px;
+  	    height: 60px;
+  	    border-radius: 60px;
+  	  }
+  	}
+  }
 </style>
--- src/views/growUp/article/commentList.vue
+++ src/views/growUp/article/commentList.vue
@@ -51,51 +51,36 @@
         </el-table-column>
         <el-table-column
           prop="province"
-          label="标签"
+          label="文章状态"
           width="">
         </el-table-column>
         <el-table-column
           prop="city"
-          label="创建时间"
+          label="评论状态"
           width="">
         </el-table-column>
         <el-table-column
           prop="address"
-          label="创建人"
+          label="总点赞数"
           width="">
         </el-table-column>
         <el-table-column
           prop="zip"
-          label="引用栏目"
+          label="总评论数"
           width="">
         </el-table-column>
         <el-table-column
           prop="zip"
-          label="所属目录"
+          label="待审核数"
           width="">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="是否置顶"
-          width="">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="发布时间"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="发布状态"
-          width="120">
         </el-table-column>
         <el-table-column
           fixed="right"
           label="操作"
-          width="100">
+          width="120">
           <template slot-scope="scope">
-            <el-button @click="editClick(scope.row)" type="text" size="small">编辑</el-button>
-            <el-button @click="deleteClick(scope.row)" type="text" size="small">删除</el-button>
+            <el-button @click="editClick(scope.row)" type="text" size="small">详情</el-button>
+            <el-button @click="deleteClick(scope.row)" type="text" size="small">关闭评论</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -115,7 +100,7 @@
   export default {
     name: 'commentList',
     data() {
-	  return {
+      return {
         formInline: {
           user: '',
           region: ''
@@ -131,13 +116,13 @@
           address: '上海市普陀区金沙江路 1518 弄',
           zip: 200333
         }]
-	  }
+      }
     },
     methods: {
       onSubmit() {},
       addArtist() {},
       editClick(row) {},
-	  deleteClick(row) {}
+      deleteClick(row) {}
     }
   }
 </script>
